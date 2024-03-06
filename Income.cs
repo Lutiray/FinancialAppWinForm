c77d57d7b3fad63abfc166f45608c4164228ea78
Natalia Eremina <entalia663@gmail.com>
2024-03-06 16:36:12 +0100

    Add transaction Id

---
+++ Income.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonalFinanceApp
+{
+    // <summary>
+    /// Class Income.
+    /// </summary>
+    public class Income : Transaction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Income"/> class.
+        /// </summary>
+        /// <param name="amount">Income amount.</param>
+        /// <param name="date">Data of receipt of income.</param>
+        public Income(int id, decimal amount, DateTime date) : 
+            base(id, amount, date)
+        {
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceApp
+{
+    /// <summary>
+    /// Class for transaction.
+    /// </summary>
+    public class Transaction
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the amount of transaction.
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of transaction.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Transaction"/> class.
+        /// </summary>
+        /// <param name="id">Transaction id.</param>
+        /// <param name="amount">Transaction amount.</param>
+        /// <param name="date">Date of the transaction.</param>
+        protected Transaction(int id, decimal amount, DateTime date)
+        {
+            Id = id;
+            Amount = amount;
+            Date = date;
+        }
+    }
+}
